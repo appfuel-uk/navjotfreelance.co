3cd35e7d671353bb69a5e985e59c44b18254bd18
Parminder Klair <perminder.klair@gmail.com>
2018-03-11 21:16:41 +0530

    added analytics

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteName: 'Navjot Singh',
   author: 'Navjot Singh',
-  description: '',
-  siteUrl: '',
-  googleAnalytics: '',
+  description: 'Navjot Singh Freelance. Graphic Designer & Content Writer.',
+  siteUrl: 'https://www.navjotfreelance.co/',
+  googleAnalytics: 'UA-1390187-38',
   backgroundColor: '#e0e0e0',
   themeColor: '#c62828',
 };
